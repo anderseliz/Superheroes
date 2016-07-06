770dce63ab58063301f44a38380c27b4f7f3100c
anderseliz <andy_eliz.sali@hotmail.com>
2016-07-05 21:42:50 -0500

    Pagina como clase en body

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,13 @@
 class ExperimentsController < ApplicationController
   def page1
+  	@body_class = 'page1'
   end
 
   def page2
+  	@body_class = 'page2'
   end
 
   def page3
+  	@body_class = 'page3'
   end
 end
